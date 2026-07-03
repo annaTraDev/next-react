248e1f052454c411827aeb0965159a7dd38f062d
Anna Trapczynska <annatrapczynska@outlook.com>
2026-07-02 22:33:35 -0700

    Fixed some bug by Anna

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { Layout } from "../components/Layout"
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  )
+}
+
+export default Home
